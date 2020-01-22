2b88097b5c7f28251ea498302fd5cdfc24b4f870
Adam Powell <adampowell@adams-imac.home>
2020-01-22 15:56:01 +0000

    First commit

---
+++ src/components/SaveScoreForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useFirebase } from './Firebase/FirebaseContext';
+
+export default function SaveScoreForm({ score, scoreSaved }) {
+  const [username, setUsername] = useState('');
+  const firebase = useFirebase();
+
+  const onUsernameChange = e => {
+    const updatedUsername = e.target.value;
+    setUsername(updatedUsername);
+  };
+
+  const saveHighScore = e => {
+    e.preventDefault();
+    const record = {
+      name: username,
+      score
+    };
+
+    firebase.scores().push(record, () => {
+      scoreSaved();
+    });
+  };
+
+  return (
+    <div className='container'>
+      <h1>Score: {score}</h1>
+      <form onSubmit={saveHighScore}>
+        <input
+          type='text'
+          name='username'
+          id='username'
+          placeholder='username'
+          vale='{username}'
+          onChange={onUsernameChange}
+        />
+        <button type='submit' className='btn'>
+          Save
+        </button>
+      </form>
+      <Link to='/' className='btn'>
+        Go back to home
+      </Link>
+    </div>
+  );
+}
